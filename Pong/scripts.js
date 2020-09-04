35649794230a654754c905a8faf8b8760052d9c7
kall0m <k.madjunov@gmail.com>
2020-09-04 19:35:01 +0200

    [up] start doing Pong with canvas; draw a simple paddle

--- Pong/scripts.js
+++ Pong/scripts.js
@@ -1,93 +1,22 @@
-const BG = 1;
-const BALL = 2;
-const BORDER = 3;
-const PADDLE1 = 4;
-const PADDLE2 = 5;
-const OUT = 6;
+var context;
 
-var field = [
-    [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 6],
-    [6, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 6],
-    [6, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6],
-    [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-];
-
-var ball = {
-    x: 11,
-    y: Math.floor(Math.random() * field.length),
-    xSpeed: 5,
-    ySpeed: 5,
+var paddle = {
+    x: 0,
+    y: 0,
+    dx: 5,
+    dy: 5,
+    w: 20,
+    h: 100,
 };
 
-function drawField() {
-    document.getElementById("scene").innerHTML = "";
+function init() {
+    context = game.getContext("2d");
 
-    for (let y = 0; y < field.length; y++) {
-        for (let x = 0; x < field[y].length; x++) {
-            if (field[y][x] === BG) {
-                document.getElementById("scene").innerHTML +=
-                    "<div class='tile bg'></div>";
-            } else if (field[y][x] === BORDER) {
-                document.getElementById("scene").innerHTML +=
-                    "<div class='tile border'></div>";
-            } else if (field[y][x] === BALL) {
-                document.getElementById("scene").innerHTML +=
-                    "<div class='tile ball'></div>";
-            } else if (field[y][x] === PADDLE1 || field[y][x] === PADDLE2) {
-                document.getElementById("scene").innerHTML +=
-                    "<div class='tile paddle'></div>";
-            }
-        }
-
-        document.getElementById("scene").innerHTML += "<br/>";
-    }
+    drawPaddle(paddle);
 }
 
-function startGame() {
-    document.onkeydown = function (e) {
-        let keyPressed = e.keyCode;
-
-        if (keyPressed === 32) {
-            //space bar pressed
-
-            //field[ball.y][ball.x] = BALL;
-
-            if (field[ball.y + 1][ball.x] !== OUT) {
-                field[ball.y][ball.x] = BG;
-                field[--ball.y][++ball.x] = BALL;
-            } else if (field[ball.y - 1][ball.x] !== OUT) {
-                field[ball.y][ball.x] = BG;
-                field[++ball.y][++ball.x] = BALL;
-            } else if (field[ball.y][ball.x + 1] !== OUT) {
-                field[ball.y][ball.x] = BG;
-                field[++ball.y][--ball.x] = BALL;
-            } else if (field[ball.y][ball.x - 1] !== OUT) {
-                field[ball.y][ball.x] = BG;
-                field[++ball.y][++ball.x] = BALL;
-            }
-
-            drawField();
-            //}
-        }
-    };
+function drawPaddle(paddle) {
+    context.beginPath();
+    context.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+    context.stroke();
 }
-
-drawField();
-startGame();
