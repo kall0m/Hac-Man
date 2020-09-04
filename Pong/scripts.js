679e03ad41a75938bedea9567522cf4cedba66bd
kall0m <k.madjunov@gmail.com>
2020-09-04 20:48:03 +0200

    [up] add paddle movement up and down when W and S keys are pressed, respectively

--- Pong/scripts.js
+++ Pong/scripts.js
@@ -1,22 +1,73 @@
-var context;
+const S_KEY = 83;
+const W_KEY = 87;
+
+var field;
 
 var paddle = {
     x: 0,
     y: 0,
     dx: 5,
-    dy: 5,
-    w: 20,
-    h: 100,
+    dy: 10,
+    width: 20,
+    height: 100,
 };
 
 function init() {
-    context = game.getContext("2d");
+    field = {
+        context: pongCanvas.getContext("2d"),
+        x: 0,
+        y: 0,
+        width: pongCanvas.width,
+        height: pongCanvas.height,
+    };
 
     drawPaddle(paddle);
+    startGame();
 }
 
 function drawPaddle(paddle) {
-    context.beginPath();
-    context.rect(paddle.x, paddle.y, paddle.w, paddle.h);
-    context.stroke();
+    field.context.beginPath();
+    field.context.rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    field.context.stroke();
+}
+
+function startGame() {
+    document.onkeydown = function (e) {
+        let keyPressed = e.keyCode;
+
+        if (keyPressed === S_KEY) {
+            movePaddleDown(paddle);
+        } else if (keyPressed === W_KEY) {
+            movePaddleUp(paddle);
+        }
+    };
+}
+
+function movePaddleDown(paddle) {
+    if (paddle.y + paddle.height < field.height) {
+        clearPreviousPaddle(paddle);
+
+        paddle.y += paddle.dy; //move paddle down by incrementing the y coordinate with dy
+
+        drawPaddle(paddle);
+    }
+}
+
+function movePaddleUp(paddle) {
+    if (paddle.y > field.y) {
+        clearPreviousPaddle(paddle);
+
+        paddle.y -= paddle.dy; //move paddle up by decrementing the y coordinate with dy
+
+        drawPaddle(paddle);
+    }
+}
+
+function clearPreviousPaddle(paddle) {
+    field.context.clearRect(
+        0,
+        0,
+        paddle.width + 1, //width of paddle + 1 px for the outline
+        field.height
+    );
 }
