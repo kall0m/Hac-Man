2ccf0a72c9a40792b506fe8d70890ac591d8f9f4
kall0m <k.madjunov@gmail.com>
2020-09-05 17:07:50 +0200

    [up] create class Ball that inherits class Asset; draw a ball on the canvas

--- Pong/scripts.js
+++ Pong/scripts.js
@@ -8,12 +8,13 @@ const CANVAS_X = 0;
 const CANVAS_Y = 0;
 
 class Asset {
-    constructor(x, y, dx, dy, context) {
+    constructor(x, y, dx, dy, context, color) {
         this._x = x;
         this._y = y;
         this._dx = dx;
         this._dy = dy;
         this._context = context;
+        this._color = color;
     }
 
     get x() {
@@ -56,6 +57,14 @@ class Asset {
         this._context = context;
     }
 
+    get color() {
+        return this._color;
+    }
+
+    set color(color) {
+        this._color = color;
+    }
+
     draw() {}
 
     clear(x, y, width, height) {
@@ -64,8 +73,8 @@ class Asset {
 }
 
 class Paddle extends Asset {
-    constructor(x, y, dx, dy, width, height, context) {
-        super(x, y, dx, dy, context);
+    constructor(x, y, dx, dy, width, height, context, color) {
+        super(x, y, dx, dy, context, color);
         this._width = width;
         this._height = height;
     }
@@ -86,7 +95,7 @@ class Paddle extends Asset {
         this._height = height;
     }
 
-    //draw the paddle with its given x & y coordinates and width & height
+    //draw a paddle with its given x & y coordinates and width & height on the canvas
     draw() {
         this._context.beginPath();
         this._context.rect(this._x, this._y, this._width, this._height);
@@ -121,8 +130,91 @@ class Paddle extends Asset {
     }
 }
 
+class Ball extends Asset {
+    constructor(
+        x,
+        y,
+        dx,
+        dy,
+        radius,
+        startAngle,
+        endAngle,
+        anticlockwise,
+        context,
+        color
+    ) {
+        super(x, y, dx, dy, context, color);
+        this._radius = radius;
+        this._startAngle = startAngle;
+        this._endAngle = endAngle;
+        this._anticlockwise = anticlockwise;
+    }
+
+    get radius() {
+        return this._radius;
+    }
+
+    set radius(radius) {
+        this._radius = radius;
+    }
+
+    get startAngle() {
+        return this._startAngle;
+    }
+
+    set startAngle(startAngle) {
+        this._startAngle = startAngle;
+    }
+
+    get endAngle() {
+        return this._endAngle;
+    }
+
+    set endAngle(endAngle) {
+        this._endAngle = endAngle;
+    }
+
+    get anticlockwise() {
+        return this._anticlockwise;
+    }
+
+    set anticlockwise(anticlockwise) {
+        this._anticlockwise = anticlockwise;
+    }
+
+    //draw a circle with its given radius and x & y coordinates on the canvas and fill it with its given color
+    draw() {
+        this._context.beginPath();
+        this._context.fillStyle = this._color;
+        this._context.arc(
+            this._x,
+            this._y,
+            this._radius,
+            this._startAngle,
+            this._endAngle,
+            this._anticlockwise
+        );
+        this._context.fill();
+    }
+}
+
 function init() {
     var paddle1 = new Paddle(0, 0, 5, 10, 20, 100, CANVAS);
+
+    var ball = new Ball(
+        100,
+        200,
+        5,
+        5,
+        20,
+        0,
+        Math.PI * 2,
+        true,
+        CANVAS,
+        "#000000"
+    );
+
+    ball.draw();
 
     paddle1.draw();
     startGame(paddle1);
